4b33a03148d38c02103c696ba46217f0be4aecc4
Tom Schönmann <tom@wild.as>
2026-02-28 13:42:24 +0100

    chore: generate types

--- apps/signatures/src/app/(payload)/admin/importMap.js
+++ apps/signatures/src/app/(payload)/admin/importMap.js
@@ -5,10 +5,9 @@ import { default as default_8744d3ceae8073a5512bb35ad9b0446b } from '../../../co
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 export const importMap = {
-  '/admin/components/SignaturePreviewAfterInput#SignaturePreviewAfterInput':
-    SignaturePreviewAfterInput_aed5cdf5dc40b2240b85f14c0600a15e,
-  '/components/Logo#Logo': Logo_919492c8e60179286a0f183c0a5b652e,
-  '/components/LocalIpWidget#default': default_6b53be4205a76e09bcff35075eca4ab7,
-  '/components/NetworkInfoWidget#default': default_8744d3ceae8073a5512bb35ad9b0446b,
-  '@payloadcms/next/rsc#CollectionCards': CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1,
+  "/admin/components/SignaturePreviewAfterInput#SignaturePreviewAfterInput": SignaturePreviewAfterInput_aed5cdf5dc40b2240b85f14c0600a15e,
+  "/components/Logo#Logo": Logo_919492c8e60179286a0f183c0a5b652e,
+  "/components/LocalIpWidget#default": default_6b53be4205a76e09bcff35075eca4ab7,
+  "/components/NetworkInfoWidget#default": default_8744d3ceae8073a5512bb35ad9b0446b,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
